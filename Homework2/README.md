844f29b9c44a3a60260d99a75809d6c1bd501b0a
Andrei Cristian <andreicristian6@protonmail.com>
2019-02-28 12:23:03 +0200

    must redo Lemniscate of Bernoulli

---
+++ Homework2/README.md
@@ -0,0 +1,17 @@
+https://profs.info.uaic.ro/~ghirvu/gpc/tema2/
+
+based on: https://profs.info.uaic.ro/~ghirvu/gpc/tema2/t02p1.cpp
+Drawing graphs using OpenGL:
+
+Press "1" for Concoid of Nicomedes;(from example);
+Press "2" for |sin x| * e^(-sin x), x in [0.8 * pi](from example);
+Press "3" for f(x)=1 if x=0, d(x)/x if x in (0,100], where d(x) is the distance from x to the nearest integer;
+Press "4" for Pascal's Snail*;
+Press "5" for Trisectrix of Longchamps*;
+Press "6" for Cycloid*;
+Press "7" for Epicycloid*;
+Press "8" for Hypocycloid*;
+Press "9" for Lemniscate of Bernoulli*;
+Press "0" for Logarithmic Spiral*;
+
+* means you can adjust the parameters of the functions by editing the parameter used in the call of the function (see Display(void) function).
--- Homework2/main.cpp
+++ Homework2/main.cpp
@@ -47,7 +47,7 @@ void Display1() {
    ymax = (fabs(ymax) > fabs(ymin)) ? fabs(ymax) : fabs(ymin);
 
    // afisarea punctelor propriu-zise precedata de scalare
-   glColor3f(1,0.1,0.1); // rosu
+   glColor3f(0.1,0.1,1); // rosu
    glBegin(GL_LINE_STRIP); 
    for (double t = - pi/2 + ratia; t < pi / 2; t += ratia) {
       double x1, y1, x2, y2;
@@ -82,7 +82,7 @@ void Display2() {
    double ratia = 0.05;
 
    // afisarea punctelor propriu-zise precedata de scalare
-   glColor3f(1,0.1,0.1); // rosu
+   glColor3f(0.1,0.1,1); // rosu
    glBegin(GL_LINE_STRIP); 
    for (double x = 0; x < xmax; x += ratia) {
       double x1, y1;
@@ -94,6 +94,148 @@ void Display2() {
    glEnd();
 }
 
+
+//y same; distance = sqrt(pow((x1-x2),2));
+void Display3(){
+    double ratio=0.05;
+    double x,y;
+    x=y=0;
+    glColor3f(0.1,0.1,1);
+    glBegin(GL_LINE_STRIP);
+    glVertex2f(0,1);
+    for (x=ratio;x<=100;x+=ratio){
+	y=sqrt(pow(x-round(x),2))/x;
+	glVertex2d(x/25,y);
+    }
+    glEnd();
+}
+
+void Display4(double a, double b){
+    double pi=4*atan(1);
+    double ratio=0.05;
+
+    glColor3f(0.1,0.1,1);
+    glBegin(GL_LINE_STRIP);
+    for (double t=-pi+ratio;t<pi;t+=ratio){
+	double x1=2*(a*cos(t)+b)*cos(t);
+	double y1=2*(a*cos(t)+b)*sin(t);
+
+	glVertex2f(x1,y1);
+    }
+    glEnd();
+}
+
+double LCx(double a, double t){
+    return (a/(4*0.5*(cos(2*t)+1)-3));
+}
+
+double LCy(double a, double t){
+    return ((a*tan(t))/(4*0.5*(cos(2*t)+1)-3));
+}
+
+void Display5(double a){
+    double pi=4*atan(1);
+    double ratio=0.05;
+    glColor3f(0.1,0.1,1);
+    glBegin(GL_LINE_STRIP);
+    double pi6=pi/6;double mpi6=-pi6;
+    for (double t=-pi/2+ratio;t<pi/2;t+=ratio){
+	if (t!=mpi6&&t!=pi6){
+	    glVertex2f(LCx(a,t),LCy(a,t));
+	}
+    }
+    /*
+    for (double t=mpi6+ratio;t<pi6;t+=ratio){
+    
+	glVertex2f(LCx(a,t),LCy(a,t));
+    }
+
+    for (double t=pi6+ratio;t<pi/2;t+=ratio){
+
+	glVertex2f(LCx(a,t),LCy(a,t));
+    }
+    */
+    glEnd();
+
+}
+
+void Display6(double a, double b){
+	
+	double ratio=0.05;
+	double x,y;
+	x=y=0;
+    glColor3f(0.1,0.1,1);
+    glBegin(GL_LINE_STRIP);
+	for (double t=-10;t<=10;t+=ratio){
+		x=a*t-b*sin(t);
+		y=a-b*cos(t);
+		glVertex2f(x,y);
+	}
+	glEnd();
+}
+
+void Display7(double R, double r){
+	double ratio=0.05;
+	double pi2=atan(1)*8;
+	double x,y;	x=y=0;
+    glColor3f(0.1,0.1,1);
+    glBegin(GL_LINE_STRIP);
+	for(double t=0;t<=pi2;t+=ratio){
+		x=(R+r)*cos(r/R*t)-r*cos(t+r/R*t);
+		y=(R+r)*sin(r/R*t)-r*sin(t+r/R*t);
+		glVertex2f(x,y);
+	}
+	glEnd();	
+}
+
+void Display8(double R, double r){
+	double ratio=0.05;
+	double pi2=atan(1)*8;
+	double x,y;	x=y=0;
+    glColor3f(0.1,0.1,1);
+    glBegin(GL_LINE_STRIP);
+	for (double t=0;t<=pi2;t+=ratio){
+		x=(R-r)*cos(r/R*t)-r*cos(t-r/R*t);
+		y=(R-r)*sin(r/R*t)-r*sin(t-r/R*t);
+		glVertex2f(x,y);
+	}
+	glEnd();
+}
+
+double TransformX(double r, double t){
+	return r*cos(t);
+}
+
+double TransformY(double r, double t){
+	return r*sin(t);
+}
+
+void Display9(double a){
+	double ratio=0.05;
+	double r,t; r=t=0;
+	double pi4=atan(1);
+
+    glColor3f(0.1,0.1,1);
+    glBegin(GL_LINE_STRIP);
+	for (t=-pi4+ratio;t<pi4;t+=ratio){
+		r=a*sqrt(2*cos(2*t));
+		glVertex2f(TransformX(r,t),TransformY(r,t));
+		glVertex2f(TransformX(-r,t),TransformY(-r,t));
+	}
+	glEnd();
+}
+
+void Display10(double a){
+	double r,t; r=t=0;
+	double ratio=0.05;
+    glColor3f(0.1,0.1,1);
+    glBegin(GL_LINE_STRIP);
+	for (t=ratio;t<=99;t+=ratio){
+		r=a*exp(1+t);
+		glVertex2f(TransformX(r,t),TransformY(r,t));
+	}
+	glEnd();
+}
 
 void Init(void) {
 
@@ -116,6 +258,30 @@ void Display(void) {
    case '2':
       Display2();
       break;
+   case '3':
+      Display3();
+      break;
+   case '4':
+      Display4(0.3,0.2);
+      break;
+   case '5':
+      Display5(0.2);
+      break;
+   case '6':
+      Display6(0.1,0.2);
+      break;
+   case '7':
+      Display7(0.1,0.3);
+      break;
+   case '8':
+      Display8(0.1,0.3);
+      break;
+   case '9':
+      Display9(0.4);
+      break;
+	case '0':
+	  Display10(0.02);
+	  break;	  
    default:
       break;
    }
